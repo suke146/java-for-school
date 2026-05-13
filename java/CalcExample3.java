1b11200c778185c2200826a88c0aef3abca6d3a4
suke146 <2510016@i-seifu.jp>
2026-05-13 11:45:56 +0900

    とりまついか

---
+++ java/CalcExample3.java
@@ -0,0 +1,14 @@
+public class CalcExample3 {
+    public static void main(String args[]) {
+        int i;
+        i = 11;
+        i++;
+        i /= 2;
+        System.out.println("iの値は" + i);
+
+        int j;
+        j = i * i;
+        System.out.println("jの値は" + j);
+        
+    }
+}
